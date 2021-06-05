c8f015422fd172c23f131f38be7b62d55f6a382e
kissingfire123 <1322591040@qq.com>
2021-06-06 01:08:14 +0800

    <feat> [04_OpenGL_Texture] add lesson-04 openGL_texture project
    /*纹理有4种选择,下面任选一种放出来可以看到效果,修改fragment-shader*/
    /*效果1: 纯色的矩形*/
    /*效果2: 加载1个纹理texture1, 一个木箱子*/
    /*效果3: 加载1个纹理texture1,且4个vertex自带color，插值后一个彩色的木箱子*/
    /*效果4: 加载2个纹理texture1和texture2,且纹理2的透明度可调*/

---
+++ 04_OpenGL_Texture/Src/Vertex.glsl
@@ -0,0 +1,13 @@
+#version 330 core 
+layout (location = 0) in vec3 position;/*接收来自VBO的数据,并且自行命名*/
+layout (location = 1) in vec3 color;
+layout (location = 2) in vec2 texCoord;
+
+out vec3 ourColor;
+out vec2 TexCoord;
+void main() 
+{
+    gl_Position = vec4(position,1.0f); 
+	ourColor = color;
+	TexCoord = vec2(texCoord.x, 1.0f - texCoord.y);/*上下翻转，可以改变纹理的朝向*/
+} 
