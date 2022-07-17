1b452794c73243f3652c1d0e2cd9be34d062ee0d
kissingfire123 <lwy13697320205@163com>
2022-07-17 16:13:26 +0800

    <feature> [09_OpenGL_LightBasics]  实现漫反射效果

--- 09_OpenGL_LightBasics/Src/Fragment.glsl
+++ 09_OpenGL_LightBasics/Src/Fragment.glsl
@@ -6,9 +6,21 @@ uniform vec3 lightPos;
 uniform vec3 objectColor;
 uniform vec3 lightColor;
 
+in vec3 Normal;
+in vec3 FragPos;
+
 void main() 
 {
+
+	// calculate diffuse light
+	vec3 norm = normalize(Normal);
+	vec3 lightDir = normalize(lightPos - FragPos);
+	float diff = max( dot(norm, lightDir), 0.0f );
+	vec3 diffuse = diff * lightColor;
+
+	// ambient light
 	float ambientStrength = 0.1f;
 	vec3 ambient = ambientStrength * lightColor;
-	color = vec4(ambient*objectColor,1.0f);
+
+	color = vec4((ambient + diffuse)*objectColor,1.0f);
 }
--- 09_OpenGL_LightBasics/Src/Vertex.glsl
+++ 09_OpenGL_LightBasics/Src/Vertex.glsl
@@ -6,6 +6,7 @@ layout (location = 0) in vec3 position;/*接收来自VBO的数据,并且自行命名*/
 layout (location = 1) in vec3 normal;/*接收来自VBO的数据,并且自行命名*/ 
 
 out vec3 Normal;
+out vec3 FragPos;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -14,6 +15,7 @@ uniform mat4 projection;
 void main() 
 {
     /*  矩阵生效注意从右向左读  */
-     gl_Position = projection * view * model * vec4(position, 1.0f); 
+    gl_Position = projection * view * model * vec4(position, 1.0f); 
     Normal = normal;
+	FragPos = vec3(model * vec4(position,1.0f));
 } 
